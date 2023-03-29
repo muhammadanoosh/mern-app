7dfe2f9337804291cb5ef95cb7f88e05bbf3e70b
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-03-29 20:39:28 +0500

    Tasklist made

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { store } from "./store";
+import  ReactDOM  from "react-dom";
+import {Main} from "./components/Main";
+
+
+ReactDOM.render(
+    <Main />,
+     document.getElementById("app")
+);
